4b640ce7b15fed0eef5a84f1fad3dedddaa3d438
spudstr <angryceo@gmail.com>
2017-08-21 18:41:46 -0400

    weffe

--- src/server/statistics.ts
+++ src/server/statistics.ts
@@ -58,7 +58,8 @@ export class EWMATargetPositionCalculator {
     console.warn(new Date().toISOString().slice(11, -1), 'SMA3', 'Value' , (this._SMA3[0] * 100 / this._SMA3[2]) );
     console.warn(new Date().toISOString().slice(11, -1), 'SMA3', 'Safety Percent' , -(100/params.safetyP) );
   }
-  console.warn(new Date().toISOString().slice(11, -1), 'SMA3', 'Value' , (this._SMA3[0] * 100 / this._SMA3[2]) );
+  
+  console.warn(new Date().toISOString().slice(11, -1), 'SMA3', 'Value' , ((this._SMA3[0] * 100 / this._SMA3[2]) -100 ) );
   console.warn(new Date().toISOString().slice(11, -1), 'SMA3', 'Safety Mode' , params.safemode[params.mSafeMode] );
 /* exit safey modes
     if(
