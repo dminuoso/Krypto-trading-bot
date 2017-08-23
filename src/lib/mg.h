c4570acad8b6a72afc736de70ee064a10e97fa3e
spudstr <angryceo@gmail.com>
2017-08-22 21:08:58 -0400

    safety debug

--- src/lib/mg.h
+++ src/lib/mg.h
@@ -115,6 +115,12 @@ namespace K {
 
       if (  mGWSMA33.back() * 100 / mGWSMA33.front() - 100 >  qpRepo["safetyP"].get<double>()/100 )
       {
+          // activate Safety, Safety buySize
+          mSafeMode qpRepo["mSafeMode"] = mSafeMode.buy;
+          printf("SMA33 First Value: %f  Second Value %f", mGWSMA33.back(), mGWSMA33.front() );
+      }
+      if (  mGWSMA33.back() * 100 / mGWSMA33.front() - 100 <  qpRepo["safetyP"].get<double>()/100 )
+      {
           printf("SMA33 First Value: %f  Second Value %f", mGWSMA33.back(), mGWSMA33.front() );
       }
 //  if (((this._SMA33[this._SMA33.length-1] * 100 / this._SMA33[0]) - 100) > (params.safetyP/100))
