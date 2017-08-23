3dde3f4eb6d57e92e91bb9820e9813faab2ce907
spudstr <angryceo@gmail.com>
2017-08-22 21:27:43 -0400

    wefef

--- src/lib/mg.h
+++ src/lib/mg.h
@@ -122,14 +122,14 @@ namespace K {
            qpRepo["mSafeMode"] = (int)mSafeMode::buy;
            qpRepo["safetimestart"] = (int)SMA33STARTTIME;
           printf("SMA33 Buy Mode Active  First Value: %f  Last Value %f safetyPercent: %f", mGWSMA33.back(), mGWSMA33.front(), qpRepo["safetyP"].get<double>()/100);
-          printf("SMA33 Start Time started at: %f", qpRepo["safetimestart"].get<int>);
+          printf("SMA33 Start Time started at: %f", qpRepo["safetimestart"].get<int>());
       }
       if (  mGWSMA33.back() * 100 / mGWSMA33.front() - 100 <  qpRepo["safetyP"].get<double>()/100 )
       {
           qpRepo["mSafeMode"] = (int)mSafeMode::sell;
           qpRepo["safetimestart"] = (int)SMA33STARTTIME;
           printf("SMA33 Sell Mode Active: First Value: %f  Last Value %f safetyPercent: %f", mGWSMA33.back(), mGWSMA33.front(),qpRepo["safetyP"].get<double>()/100 );
-          printf("SMA33 Start Time started at: %f", qpRepo["safetimestart"].get<int>);
+          printf("SMA33 Start Time started at: %f", qpRepo["safetimestart"].get<int>());
       }
 
 
