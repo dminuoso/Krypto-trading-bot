7222cd68638a89fb583790609273baf22d218d8d
spudstr <angryceo@gmail.com>
2017-08-23 00:04:19 -0400

    tst

--- src/lib/mg.h
+++ src/lib/mg.h
@@ -117,7 +117,7 @@ static void _mgTBP(const FunctionCallbackInfo<Value>& args) {
          qpRepo["aspvalue"] = newTargetPosition;
         printf("ASP: value: %f\n", newTargetPosition);
         printf("ASP: value: %f\n", qpRepo["aspvalue"].get<double>());
-        printf("ASP: testvalue: %f\n", (newShort * 100/ newLong)  );
+        printf("ASP: testvalue: %f\n", ((newShort * 100/ newLong)  - 100)  );
 
 
         if (newTargetPosition > 1) newTargetPosition = 1;
