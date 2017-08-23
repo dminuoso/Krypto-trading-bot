588a6b0232ed11dad21625a5d8c26d5910d4e529
spudstr <angryceo@gmail.com>
2017-08-22 20:12:26 -0400

    s3

--- src/lib/mg.h
+++ src/lib/mg.h
@@ -115,7 +115,7 @@ namespace K {
 
 
 
-        if ( (mGWSMA33.end()-1 * 100 / mGWSMA33.begin() - 100 ) > 5 )
+        if ( (( mGWSMA33.end()-1 * 100) / mGWSMA33.begin() - 100 ) > 5 )
         {
     //  params.mSafeMode = Models.mSafeMode.buy;
     //  params.safetimestart = currenttime.getTime();
