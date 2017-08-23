16d7431131b78913307e56301ed360ff8148f179
spudstr <angryceo@gmail.com>
2017-08-23 09:08:08 -0400

    debug 3

--- src/lib/mg.h
+++ src/lib/mg.h
@@ -202,10 +202,10 @@ static void _mgTBP(const FunctionCallbackInfo<Value>& args) {
         }
             printf("Debug1111\n");
          printf("safetyactive: %d safetynet: %d safemode: %d\n", qpRepo["safetyactive"].get<bool>(), qpRepo["safetynet"].get<bool>(), qpRepo["mSafeMode"].get<int>() );
-        if( qpRepo["safetyactive"].get<bool>() && qpRepo["safetynet"].get<bool>() && (mSafeMode)qpRepo["mSafeMode"].get<int>() == mSafeMode::buy  )
+        if( qpRepo["safetyactive"].get<bool>() && qpRepo["safetynet"].get<bool>() && qpRepo["mSafeMode"].get<int>() == mSafeMode::buy  )
         {
                 newTargetPosition = 1;
-        } else if( qpRepo["safetyactive"].get<bool>() && qpRepo["safetynet"].get<bool>() && (mSafeMode)qpRepo["mSafeMode"].get<int>() == mSafeMode::sell  )
+        } else if( qpRepo["safetyactive"].get<bool>() && qpRepo["safetynet"].get<bool>() && qpRepo["mSafeMode"].get<int>() == mSafeMode::sell  )
         {
                 newTargetPosition = -1;
         }
