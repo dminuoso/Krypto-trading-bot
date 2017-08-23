dd32ac91784c685fb60308750daa434c151a796f
spudstr <angryceo@gmail.com>
2017-08-22 23:22:30 -0400

    wefwefe

--- src/lib/mg.h
+++ src/lib/mg.h
@@ -112,7 +112,9 @@ static void _mgTBP(const FunctionCallbackInfo<Value>& args) {
         }
         newTargetPosition = ((newShort * 100/ newLong) - 100) * (1 / qpRepo["ewmaSensiblityPercentage"].get<double>());
         qpRepo["aspvalue"] = newTargetPosition;
-        
+        printf("ASP: value: %f\n", newTargetPosition);
+        printf("ASP: testvalue: %f\n" ((newShort * 100/ newLong) - 100) * (1 / qpRepo["ewmaSensiblityPercentage"].get<double>()));
+
 
         if (newTargetPosition > 1) newTargetPosition = 1;
         else if (newTargetPosition < -1) newTargetPosition = -1;
