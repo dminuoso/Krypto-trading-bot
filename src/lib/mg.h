4ff505cd2d2f1633c3df7c2167c85dc367d95302
spudstr <angryceo@gmail.com>
2017-08-23 09:33:30 -0400

    wefefef

--- src/lib/mg.h
+++ src/lib/mg.h
@@ -203,7 +203,7 @@ static void _mgTBP(const FunctionCallbackInfo<Value>& args) {
             printf("Debug1111\n");
             printf("safetyactive: %d \n",qpRepo["safetyactive"].get<bool>());
             printf("safetyactive: %d \n", qpRepo["safetynet"].get<bool>());
-            printf("safemode %d\n", (mSafeMode)qpRepo["mSafeMode"].get<int>() );
+            printf("safemode %s\n", qpRepo["mSafeMode"].get<string>() );
         // printf("safetyactive: %d safetynet: %d safemode: %d\n", qpRepo["safetyactive"].get<bool>(), qpRepo["safetynet"].get<bool>(), qpRepo["mSafeMode"].get<int>() );
         if( qpRepo["safetyactive"].get<bool>() == true and qpRepo["safetynet"].get<bool>() == true )
         {
