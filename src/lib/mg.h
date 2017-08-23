39ad2c64cac3638327655c94abf4cb19bd99aa00
spudstr <angryceo@gmail.com>
2017-08-23 01:26:41 -0400

    wefef

--- src/lib/mg.h
+++ src/lib/mg.h
@@ -173,7 +173,8 @@ static void _mgTBP(const FunctionCallbackInfo<Value>& args) {
                 {
                   printf("debug2\n");
                       //  if( (mGWSMA33.back() < mGWSMA33.at(mGWSMA33.size() - qpRepo["safetytime"].get<int>()) ) && (qpRepo["safetyduration"].get<unsigned long int>() >= (qpRepo["safetimeOver"].get<unsigned long int>() * 60000)))
-                        if( mGWSMA33.back() < mGWSMA33.at(mGWSMA33.size() - 2) )  
+                      printf("arraySize: %f", mGWSMA33.size() );
+                        if( mGWSMA33.back() < mGWSMA33.at(2) )
                         {
                           printf("debug3\n");
                                 qpRepo["mSafeMode"] = (int)mSafeMode::unknown;
