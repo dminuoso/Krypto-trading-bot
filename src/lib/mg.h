c6e6254a5bf49d3f28e26dd289e445511ac9f04e
spudstr <angryceo@gmail.com>
2017-08-24 10:49:36 -0400

    correcting asp syntax

--- src/lib/mg.h
+++ src/lib/mg.h
@@ -336,7 +336,28 @@ static void calcSafety() {
         }
         cout <<  "ASP Evaluation: " << ((mgEwmaS * 100/ mgEwmaL) - 100) * (1 / qpRepo["ewmaSensiblityPercentage"].get<double>()) << "\n";
         cout <<  "ASP Evaluation result: " << qpRepo["aspvalue"].get<double>() << "\n";
-        if ( ( ( mgfairV > mgEwmaS) and (qpRepo["aspvalue"].get<double>() >= qpRepo["asp_high"].get<double>() ) ) || qpRepo["aspvalue"].get<double>() <= qpRepo["asp_low"].get<double>()) && qpRepo["aspactive"].get<bool>() == true) {
+        if (
+             (
+              (
+                (
+                  mgfairV > mgEwmaS
+                )
+                and
+                (
+                  (
+                    qpRepo["aspvalue"].get<double>() >= qpRepo["asp_high"].get<double>()
+                  )
+                )
+               )
+            ||
+               (
+                 (
+                    qpRepo["aspvalue"].get<double>() <= qpRepo["asp_low"].get<double>()
+                 )
+               )
+             )
+          && qpRepo["aspactive"].get<bool>() == true
+          ) {
             //  cout << "ASP high?: " << qpRepo["aspvalue"].get<double>() " >= " << qpRepo["asp_high"].get<double>() << " or asp low?: " << qpRepo["aspvalue"].get<double>() << "<= " << qpRepo["asp_low"].get<double>()) << "\n";
               cout << "ASP Active! pDiv should be set to Zero!\n";
               cout << "fv: " << mgfairV << " Current Short: " << mgEwmaS << "\n";
